3932d221e1c02cd3bde0db2f24099d6cc1a0efcc
multida <wow_da65@naver.com>
2024-11-25 23:04:25 +0900

    Add components

---
+++ components/form-input.tsx
@@ -0,0 +1,37 @@
+interface FormInputProps {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+  name: string;
+  Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+  name,
+  Icon,
+}: FormInputProps) {
+  return (
+    <div className="flex flex-col gap-2 relative">
+      {Icon && (
+        <Icon className="absolute left-2 top-2.5 h-5 w-5 text-gray-400" />
+      )}
+      <input
+        className="bg-transparent rounded-3xl w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 px-8"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        name={name}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
