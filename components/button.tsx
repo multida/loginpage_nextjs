0d18cd61817d9978e60e3ac6df2d2bbf385ba42d
multida <wow_da65@naver.com>
2024-12-14 16:58:04 +0900

    버셀 에러 수정

--- components/button.tsx
+++ components/button.tsx
@@ -8,6 +8,7 @@ export default function Button({ text, ...props }: ButtonProps) {
   const { pending } = useFormStatus();
   return (
     <button
+      {...props}
       disabled={pending}
       className="primary-btn h-10 flex items-center justify-center bg-orange-400  text-white font-bold rounded-2xl disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
     >
