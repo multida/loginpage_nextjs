0729a9900bd08165dc72e442bb6b578327dc3a7b
multida <wow_da65@naver.com>
2024-12-14 22:45:10 +0900

    트윗 버튼 수정

--- components/tweet-list.tsx
+++ components/tweet-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ListTweet from "./list-tweet";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { getMoreTweets } from "@/app/(tabs)/tweets/actions";
 
 export interface TweetListProps {
@@ -17,38 +17,17 @@ export default function TweetList({ initialTweets }: TweetListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
-  const trigger = useRef<HTMLSpanElement>(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      async (
-        entries: IntersectionObserverEntry[],
-        observer: IntersectionObserver
-      ) => {
-        const element = entries[0];
-        if (element.isIntersecting && trigger.current) {
-          observer.unobserve(trigger.current);
-          setIsLoading(true);
-          const newTweets = await getMoreTweets(page + 1);
-          if (newTweets.length !== 0) {
-            setTweets((prev) => [...prev, ...newTweets]);
-            setPage((prev) => prev + 1);
-          } else {
-            setIsLastPage(true);
-          }
-          setIsLoading(false);
-        }
-      },
-      {
-        threshold: 1.0,
-      }
-    );
-    if (trigger.current) {
-      observer.observe(trigger.current);
+  const onLoadMoreClick = async () => {
+    setIsLoading(true);
+    const newTweet = await getMoreTweets(page + 1);
+    if (newTweet.length !== 0) {
+      setPage((pre) => pre + 1);
+      setTweets((pre) => [...pre, ...newTweet]);
+    } else {
+      setIsLastPage(true);
     }
-    return () => {
-      observer.disconnect();
-    };
-  }, [page]);
+    setIsLoading(false);
+  };
   return (
     <div className="p-5 flex flex-col gap-5 h-[calc(100vh-100px)] overflow-y-auto max-w-3xl mx-auto">
       {tweets.map((tweet) => (
@@ -59,14 +38,17 @@ export default function TweetList({ initialTweets }: TweetListProps) {
           No tweets found!
         </span>
       )}
-      {!isLastPage ? (
-        <span
-          ref={trigger}
+      {isLastPage ? (
+        <span className="mx-auto block mt-10 text-center">트윗 끝!</span>
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
           className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩 중" : "Load more"}
-        </span>
-      ) : null}
+        </button>
+      )}
     </div>
   );
 }
