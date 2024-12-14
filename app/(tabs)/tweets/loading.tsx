69c1a2dd3f8cfd82ca31acc1a7463f6779e03551
multida <wow_da65@naver.com>
2024-12-14 16:54:04 +0900

    스켈레톤 이미지 삭제

--- app/(tabs)/tweets/loading.tsx
+++ app/(tabs)/tweets/loading.tsx
@@ -3,7 +3,6 @@ export default function Loading() {
     <div className="p-5 animate-pulse flex flex-col gap-5 max-w-3xl mx-auto bg-white shadow-md rounded-lg h-[calc(100vh-100px)] overflow-y-auto mt-4">
       {[...Array(10)].map((_, index) => (
         <div key={index} className="*:rounded-md flex gap-5 ">
-          <div className="size-28 bg-neutral-700" />
           <div className="flex flex-col gap-2 *:rounded-md">
             <div className="bg-neutral-700 h-5 w-40" />
             <div className="bg-neutral-700 h-5 w-20" />
